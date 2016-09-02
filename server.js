3a36555ecdb09405137da9de070e00cafee265b1
k24dizzle <zhaok24@uw.edu>
2016-09-01 21:43:04 -0700

    AHHHHH

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
-    res.send('<h1>Hello Blod</h1>');
+    res.sendFile(__dirname + '/index.html');
 });
 
 http.listen(3000, function() {
     console.log('listening on stuff');
+});
+
+io.on('connection', function(socket) {
+    console.log('blod is here');
+    socket.on('disconnect', function(){
+        console.log('blod disconnected');
+    });
 });
