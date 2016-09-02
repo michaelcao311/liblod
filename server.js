92249c780499e380ce0f566ca398058b1aa4f4f3
k24dizzle <zhaok24@uw.edu>
2016-09-01 20:47:07 -0700

    wooosh

--- server.js
+++ server.js
@@ -1,2 +1,10 @@
-console.log("Hello World");
+var app = require('express')();
+var http = require('http').Server(app);
 
+app.get('/', function(req, res) {
+    res.send('<h1>Hello Blod</h1>');
+});
+
+http.listen(3000, function() {
+    console.log('listening on stuff');
+});
