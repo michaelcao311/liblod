a6a51f0d734b8e919c224a6ad9411e572c43c7a2
michaelcao311 <caom1997@uw.edu>
2016-09-03 03:45:59 -0700

    namespaces get created, but no connection

--- server/server.js
+++ server/server.js
@@ -5,9 +5,11 @@ var path = require('path');
 var nunjucks = require('nunjucks');
 var io = require('socket.io')(http);
 
-var rooms = [];
+var roomnames = new Array();
+var rooms = new Map();
 var users = [];
-
+var namespaces = [];
+console.log(1, rooms);
 
 nunjucks.configure('views', {
   autoescape: true,
@@ -20,8 +22,7 @@ app.use(express.static(__dirname));
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/home.html', function (err) {
     if (err) console.log(err);
-    else 
-      console.log('jtb');
+    else console.log('jtb');
   });
 });
 
@@ -29,8 +30,8 @@ app.get('/reg', function(req, res) {
   var queries = req.query;
   var name = queries.name;
   var room = queries.room;
-  rooms.push(room);
-  users.push(name);
+  console.log('roomnames: ', roomnames);
+  console.log('users: ', users);
 
 
   // res.send("name: " + name + "<br>room: " + room)
@@ -41,10 +42,36 @@ http.listen(1111, function() {
   console.log('listening at localhost:1111');
 });
 
-// io.use(function(socket, next) {
-//   console.log('name: ', socket.handshake.query);
-//   next();
-// });
+io.use(function(socket, next) {
+  // console.log('query: ', socket.handshake.query);
+  next();
+});
+io.use(function(socket, next) {
+console.log(2, rooms);
+  var rName = socket.handshake.query.roomname;
+  var uName = socket.handshake.query.name;
+  // console.log('rooms: ' + rooms)
+  if (roomnames.indexOf(rName) === -1) {
+  console.log(3, rooms);
+    // console.log('rName: ', rName);
+    // console.log('indexof: ', roomnames.indexOf(rName));
+    // console.log('roomnames: ', roomnames);
+
+    roomnames.push(rName);
+    rooms[rName] = new Array();
+    rooms[rName].push(uName);
+    makeNsp(rName);
+    console.log(4, rooms);
+  } else {  
+    console.log('rName: ', rName);
+    // console.log('indexof: ', roomnames.indexOf(rName));
+    // console.log('roomnames: ', roomnames);
+    rooms[rName].push(uName);
+    console.log(5, rooms);
+  } 
+
+  console.log('namespaces: ' + namespaces);
+});
 
 io.on('connection', function(socket) {
   var id = socket.id;
@@ -59,4 +86,22 @@ io.on('connection', function(socket) {
   });
 });
 
-
+var makeNsp = function(name) {
+  console.log(1);
+  // console.log('rooms: ' + rooms);
+  var nsp = io.of('/' + name);
+  console.log(2);
+  nsp.on('connection', function(socket){
+    console.log('someone connected');
+    nsp.on('chat message', function(msg) {
+      console.log(msg);
+      io.emit('chat recieved', msg);
+    });
+    socket.on('disconnect', function() {
+      console.log('someone disconnected');
+    });
+  });
+  console.log(nsp);
+  namespaces.push(nsp);
+  
+}
