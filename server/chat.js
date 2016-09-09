c0e486da80790a27cc4744054a239eef47c1dd3d
k24dizzle <zhaok24@uw.edu>
2016-09-09 11:07:34 -0700

    basic clicking, now need to emit to server

--- server/chat.js
+++ server/chat.js
@@ -12,6 +12,26 @@ $(document).ready(function() {
         $('#playtictactoe').attr('disabled', true); 
     });
 
+    var buttons = jQuery('button[id^="sq"]');
+    var current = 'X'
+    $(buttons).on('click', function(event) {
+        var square = $(this);
+        console.log(square.text());
+        if (square.text() == ' ') {
+            console.log(square.attr('id'));
+            square.text(current);
+            if (current == 'X') {
+                square.removeClass('btn-success');
+                square.addClass('btn-danger');
+                current = 'O';
+            } else {
+                square.removeClass('btn-success');
+                square.addClass('btn-warning');
+                current = 'X';
+            }
+        }
+    });
+
     socket.on('chat recieved', function(msg, username) {
         console.log('here');
         log(username + ": " + msg);    
